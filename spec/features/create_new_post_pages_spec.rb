3aedaf8849319ec4b50ec29574e9b561994d0e0c
Matthew <meterrill@gmail.com>
2015-03-20 11:34:20 -0700

    create integration test for creating a new post and make it pass

---
+++ spec/features/create_new_post_pages_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe "the create new post process" do
+  it "creates a new post" do
+    visit posts_path
+    click_on 'Create a new post'
+    fill_in 'Title', :with => 'Test post'
+    fill_in 'Body', :with => 'Test body'
+    click_on "Create Post"
+    expect(page).to have_content 'Post'
+  end
+
+  it "gives an error when no title is entered" do
+    visit new_post_path
+    click_on 'Create Post'
+    expect(page). to have_content 'errors'
+  end
+end

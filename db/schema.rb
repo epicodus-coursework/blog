a23bffaa1b976de16ba94753b7f64a08ec5c4709
Matthew <meterrill@gmail.com>
2015-03-27 10:45:08 -0700

    add test to validate comment and associate with post, and make it pass

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150327172409) do
+ActiveRecord::Schema.define(version: 20150327174239) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20150327172409) do
     t.string   "body"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "post_id"
   end
 
   create_table "posts", force: :cascade do |t|

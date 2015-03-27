5c18bc0b2ae561070cb11f3dcd36c61d883d2d37
Matthew <meterrill@gmail.com>
2015-03-27 11:41:46 -0700

    add ability to edit and delete comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -15,6 +15,30 @@ class CommentsController < ApplicationController
     end
   end
 
+  def edit
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    if @comment.update(comment_params)
+      flash[:notice] = "Comment successfully updated!"
+      redirect_to post_path(@comment.post)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    @comment.destroy
+    flash[:notice] = "Comment successfully deleted."
+    redirect_to posts_path
+  end
+
 private
   def comment_params
     params.require(:comment).permit(:body)
